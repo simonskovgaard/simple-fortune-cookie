3667802689fe4f1f68f2d2538da5d515bf5c6c86
Luca Strahm <lustr22@student.sdu.dk>
2022-08-18 09:03:09 +0000

    test.yml 6

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -2,4 +2,5 @@
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 #docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
-docker build -t "${DockerRepo}backend:latest"
+docker build -t "${DockerRepo}backend:latest" backend/
+docker build -t "${DockerRepo}frontend:latest" frontend/
