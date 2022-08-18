3213e6580e9d743f3e04b098297d19e75f7ee3cd
Luca Strahm <lustr22@student.sdu.dk>
2022-08-18 13:25:51 +0000

    test 102

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/backend:1.0-${GIT_COMMIT::8}"
+docker push "$docker_username/backend:${GIT_COMMIT::8}"
 #docker push "$docker_username/frontend:1.0-${GIT_COMMIT::8}" &
 docker push "$docker_username/frontend:${GIT_COMMIT::8}" &
 wait
