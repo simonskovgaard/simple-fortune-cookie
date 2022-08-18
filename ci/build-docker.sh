9f0169511eccea8c6f0a7aa7c2c6c16343cc396e
Simon Skovgaard Kristensen <simok20@student.sdu.dk>
2022-08-18 13:23:22 +0000

    trying out Git SHA #11

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}backend:${GIT_COMMIT::8}" backend/
+docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend:$Tag" backend/
 docker build -t "${DockerRepo}frontend:${GIT_COMMIT::8}" frontend/
