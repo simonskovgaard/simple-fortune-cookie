efa2c1afedd87f60e6e6caebb40c058b188664d1
Simon Skovgaard Kristensen <simok20@student.sdu.dk>
2022-08-18 12:59:22 +0000

    trying out Git SHA #8

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+echo "$docker_username"
+echo "${DOCKER_USERNAME}"
 #docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}"  
 docker push "$docker_username/backend:1.0-${GIT_COMMIT::8}"
 docker push "$docker_username/frontend:1.0-${GIT_COMMIT::8}" &
