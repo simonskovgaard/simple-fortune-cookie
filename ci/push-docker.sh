8015f920988914afdaddac321e19be313b52e539
Simon Skovgaard Kristensen <simok20@student.sdu.dk>
2022-08-18 13:19:16 +0000

    trying out Git SHA #10

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -2,5 +2,5 @@
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
 docker push "$docker_username/backend:1.0-${GIT_COMMIT::8}"
 #docker push "$docker_username/frontend:1.0-${GIT_COMMIT::8}" &
-docker push "$docker_username/frontend:latest" &
+docker push "$docker_username/frontend:${GIT_COMMIT::8}" &
 wait
